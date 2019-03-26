c09f2a21ec142643c3aa12dfec9bcca92e76ef54
Mukul Aggarwal <mukul.agarwal@joshtechnologygroup.com>
2019-03-26 19:18:06 +0530

    CE-3539: Removed eyinstall script and refactoring of chef recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,17 +8,17 @@
 #
 
 script "revealcloud_install" do
-	interpreter "bash"
-	cwd
-	user "root"
-	code <<-EOH
+  interpreter "bash"
+  cwd
+  user "root"
+  code <<-EOH
     curl -s http://#{node[:copperegg][:apikey]}@api.copperegg.com/rc.sh > /tmp/revealcloud.sh
     chmod +x /tmp/revealcloud.sh
     export RC_TAG="#{node[:copperegg][:tags] || ''}"
     export RC_LABEL="#{node[:copperegg][:label] || ''}"
     export RC_PROXY="#{node[:copperegg][:proxy] || ''}"
     export RC_OOM_PROTECT="#{node[:copperegg][:oom_protect] || ''}"
-		export RC_UUID="#{node[:copperegg][:override_uuid] || ''}"
-		/tmp/revealcloud.sh
-	EOH
+    export RC_UUID="#{node[:copperegg][:override_uuid] || ''}"
+    /tmp/revealcloud.sh
+  EOH
 end
